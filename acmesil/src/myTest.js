a7a935f0b057959afb32f608065fd187b2f1e1b5
jusola2 <juanjosesolano@hotmail.com>
2019-05-28 14:22:05 -0600

    update

--- acmesil/src/myTest.js
+++ acmesil/src/myTest.js
@@ -1,10 +1,49 @@
-import './myTest.css';
 
-function App() {
-  return (
-    <div className="MyTest">
-    </div>
-  );
+import{ Navbar,Nav,NavDropdown }from 'react-bootstrap';
+import React,{ Component } from 'react';
+
+class MyTest extends Component{
+
+    render(){
+        return (
+            <>
+  <Navbar bg="light">
+    <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="light">
+    <Navbar.Brand>Brand text</Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="dark">
+    <Navbar.Brand href="#home">
+      <img
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />
+      {' React Bootstrap'}
+    </Navbar.Brand>
+  </Navbar>
+</>
+          );
+        }
 }
+  
 
-export default App;
+
+export default MyTest;
